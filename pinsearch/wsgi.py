2dc9a43818727ab58c570cc18f7d7e57c9abff8e
kpurna <kpurna543@gmail.com>
2016-03-17 19:34:39 +0530

    Changes in the settings and wsgi files.

--- pinsearch/wsgi.py
+++ pinsearch/wsgi.py
@@ -21,9 +21,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pinsearch.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-# application = get_wsgi_application()
-application = Cling(get_wsgi_application())
+from whitenoise.django import DjangoWhiteNoise
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
