3dd463f0176b35d20bd9cc1fc75acc05a894f07a
kpurna <kpurna543@gmail.com>
2016-03-17 19:31:45 +0530

    Changes in the settings and wsgi files.

--- pinsearch/wsgi.py
+++ pinsearch/wsgi.py
@@ -21,8 +21,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pinsearch.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
 from dj_static import Cling
+# application = get_wsgi_application()
 application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
